795c99e91c8ab9e4ae83a3a2c83ba06d0778b077
Zac Wolff <zacharyhussein@gmail.com>
2015-03-09 16:08:03 +0000

    Initial Commit

---
+++ Login View/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Login View
+//
+//  Created by Zac Wolff on 3/9/15.
+//  Copyright (c) 2015 Zac Wolff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Login View/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Login View
+//
+//  Created by Zac Wolff on 3/9/15.
+//  Copyright (c) 2015 Zac Wolff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
