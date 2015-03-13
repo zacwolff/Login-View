6689c4ec5d62da63b51a7be5c8a77163191d14b7
Zac Wolff <zacharyhussein@gmail.com>
2015-03-13 21:02:28 +0000

    Pushing VIews Code

--- Login View/ViewController.h
+++ Login View/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)button:(id)sender;
 
 @end
 
--- Login View/ViewController.m
+++ Login View/ViewController.m
@@ -24,4 +24,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)button:(id)sender
+{
+    NSLog(@"This button works");
+    
+    UIStoryboard *myStoryboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    UIViewController *testViewController;
+    testViewController = [myStoryboard instantiateViewControllerWithIdentifier:@"firstSegue"];
+    [self.navigationController presentViewController:testViewController animated:YES completion:nil];
+}
 @end
